9ceabdba8bf59a33038b014763620251de7a8450
ConardMayes-flatiron <ConradAsatoMayes+flatironschool@gmail.com>
2021-04-12 09:40:37 -0500

    commit tree 50ad8b190ff07e6ce76955929dfe7d7e90e289dd

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+
+const HQ = 42;
+
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(HQ - someValue);
+
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+
+}
+
+function distanceTravelledInFeet(startingBlock, endingBlock) {
+    return Math.abs(startingBlock - endingBlock) * 264;
+
+}
+
+function calculatesFarePrice(start, destination){
+    let thisTrip = distanceTravelledInFeet(start,destination)
+
+    if (thisTrip < 400) {
+        return 0;
+
+    }else if (thisTrip >= 400 && thisTrip < 2000){
+        return .02 * (thisTrip - 400);
+
+    }else if (thisTrip >= 2000 && thisTrip < 2500){
+        return 25;
+
+    }else{
+        return 'cannot travel that far';
+    }
+
+}
+
+done();
